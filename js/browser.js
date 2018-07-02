e99422540950af98ff45b6dc12cf7d9d34111c53
LesterGallagher <LesterGallagher@gmail.com>
2018-07-02 15:54:23 +0200

    homescreen

--- js/browser.js
+++ js/browser.js
@@ -19,3 +19,43 @@ if (navigator.userAgent.indexOf('MSIE') !== -1
 </div>'.split('\n').join('');
 }
 
+
+var installPromptEvent;
+
+window.addEventListener('beforeinstallprompt', function(event) {
+    // Prevent Chrome <= 67 from automatically showing the prompt
+    event.preventDefault();
+    // Stash the event so it can be triggered later.
+    installPromptEvent = event;
+    // Update the install UI to notify the user app can be installed
+    var btnContainers = document.getElementsByClassName('add-to-homescreen');
+    for (var i = 0; i < btnContainers.length; i++) {
+        btnContainers[i].style.display = 'block';
+    }
+    var scrolltoElem = btnContainers[btnContainers.length - 1];
+    scrolltoElem.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+function pwaHomeScreenInstall() {
+    if (!installPromptEvent) return;
+    // Update the install UI to remove the install button
+    var btnContainers = document.getElementsByClassName('add-to-homescreen');
+    for (var i = 0; i < btnContainers.length; i++) {
+        btnContainers[i].style.display = 'none';
+    }
+    // Show the modal add to home screen dialog
+    installPromptEvent.prompt();
+    // Wait for the user to respond to the prompt
+    installPromptEvent.userChoice.then(function(choice) {
+        if (choice.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        } else {
+            console.log('User dismissed the A2HS prompt');
+        }
+        // Clear the saved prompt since it can't be used again
+        installPromptEvent = null;
+    });
+}
+
